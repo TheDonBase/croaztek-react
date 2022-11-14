5c5f35cc3426f49b432b7141b53d9cc712973255
TheDonBase <basebulldozer@gmail.com>
2022-11-14 10:15:46 +0100

    Added bunch of stuff

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -17,6 +17,21 @@ import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
+const Item = ({title, to, icon, selected, setSelected}) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+        <MenuItem 
+        active={selected === title} 
+        style={{ color:colors.grey[100] }} 
+        onClick={() => setSelected(title)} 
+        icon={icon}>
+            <Typography>{title}</Typography>
+            <Link to={to}/>
+        </MenuItem>    
+    )
+}
+
 const Sidebar = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
@@ -59,12 +74,37 @@ const Sidebar = () => {
                             <Typography variant="h3" color={colors.grey[300]}>
                                 CroazTek Admin
                             </Typography>
-                            <IconButton>
+                            <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
                                 <MenuOutlinedIcon />
                             </IconButton>
                         </Box>
                     )}
                     </MenuItem>
+
+                    {/* USER */}
+                    {!isCollapsed && (
+                      <Box mb="25px">
+                        <Box display="flex" justifyContent="center" alignItems="center">
+                            <img 
+                            alt="profile-user"
+                            width="100px"
+                            height="100px"
+                            src={`../../assets/images/placeholder.jpg`}
+                            style={{
+                            cursor: "pointer", borderRadius: "50%"
+                            }}
+                            />
+                        </Box>
+                      <Box textAlign="center">
+                          <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{m: "10px 0 0 0"}}>Trishkes (Placeholder)</Typography>
+                          <Typography variant="h5" color={colors.greenAccent[500]}>Peasant (Placeholder)</Typography>
+                      </Box>
+                  </Box>  
+                    )}
+                    {/* MENU ITEMS */}
+                    <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+                            
+                    </Box>
                 </Menu>
             </ProSidebar>
         </Box>    
