21b0dc47fd050278a67021b89aa40ab9ecd59dba
TheDonBase <basebulldozer@gmail.com>
2022-11-14 09:03:54 +0100

    initial commit

---
+++ src/data/mockData.js
@@ -0,0 +1,18 @@
+export const mockDataTeam = [
+    {
+        id: 0,
+        name: "Sebastian Andersson",
+        email: "thedonbase@croaztek.com",
+        age: 26,
+        phone: "(+46)12345678",
+        role: "developer",
+    },
+    {
+        id: 1,
+        name: "Giovanni Le Grand",
+        email: "reashetyr@croaztek.com",
+        age: 26,
+        phone: "(+31)12345678",
+        role: "developer",
+    },
+]
